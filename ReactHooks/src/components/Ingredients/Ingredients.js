56d70a4668794839f2d28f65663205d7e389478e
Daniela Porcescu <dporcescu@grundfos.com>
2020-05-26 14:46:47 +0200

    #443 userReducer() understanding and using

--- ReactHooks/src/components/Ingredients/Ingredients.js
+++ ReactHooks/src/components/Ingredients/Ingredients.js
@@ -1,13 +1,27 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
+function ingredientReducer(currentIngredients, action) {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there');
+  }
+};
+
 function Ingredients() {
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
   // using array destructuring
-  const [userIngredients, setUserIngredients] = useState([]);
+  //const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -25,14 +39,14 @@ function Ingredients() {
           amount: responseData[key].amount
         });
       }
-      setUserIngredients(loadedIngredients);
+      //setUserIngredients(loadedIngredients);
     });
     // without [] it will enter in an infinite loop decause of how useEffects work by default 
     // [] is used so useEffects will run just when there is a change
   }, []);
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
   function addIngredientHandler(ingredient) {
@@ -46,28 +60,33 @@ function Ingredients() {
       return response.json();
       // name is from firebase
     }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient }
-      ]);
+      // setUserIngredients(prevIngredients => [
+      //   ...prevIngredients,
+      //   { id: responseData.name, ...ingredient }
+      // ]);
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: responseData.name, ...ingredient }
+      });
     });
   };
 
   function removeIngredientHandler(ingredientId) {
     setIsLoading(true);
     fetch(`https://react-hooks-project-5a6d8.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE',
+      method: 'DELETE'
     }).then(response => {
       setIsLoading(false);
-      setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
+      // setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
+      dispatch({ type: 'DELETE', id: ingredientId });
     }).catch(error => {
       setError('Something went wrong!');
+      setIsLoading(false);
     });
   };
 
   function clearError() {
     setError(null);
-    setIsLoading(false);
   };
 
   return (
