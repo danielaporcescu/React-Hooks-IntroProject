e53b8acb8c25ab9838ee67f6f7b48e7eaafb9968
Daniela Porcescu <dporcescu@grundfos.com>
2020-05-26 14:14:04 +0200

    adding a spinner when something is loading

--- ReactHooks/src/components/Ingredients/Ingredients.js
+++ ReactHooks/src/components/Ingredients/Ingredients.js
@@ -7,6 +7,7 @@ import IngredientList from './IngredientList';
 function Ingredients() {
   // using array destructuring
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // to get the side effects, like a response from database
   // it is executed after every component render cycle
@@ -33,11 +34,13 @@ function Ingredients() {
   }, []);
 
   function addIngredientHandler(ingredient) {
+    setIsLoading(true);
     fetch('https://react-hooks-project-5a6d8.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
+      setIsLoading(false);
       return response.json();
       // name is from firebase
     }).then(responseData => {
@@ -49,16 +52,20 @@ function Ingredients() {
   };
 
   function removeIngredientHandler(ingredientId) {
+    setIsLoading(true);
     fetch(`https://react-hooks-project-5a6d8.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE',
     }).then(response => {
+      setIsLoading(false);
       setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
     });
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
