6eee25c526bb3efece1157a10d42618eadacb893
Daniela Porcescu <dporcescu@grundfos.com>
2020-05-26 13:25:36 +0200

    #434 uaing useEffect() and loading data

--- ReactHooks/src/components/Ingredients/Ingredients.js
+++ ReactHooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,6 +7,26 @@ import IngredientList from './IngredientList';
 function Ingredients() {
   // using array destructuring
   const [userIngredients, setUserIngredients] = useState([]);
+
+  // to get the side effects, like a response from database
+  // it is executed after every component render cycle
+  useEffect(() => {
+    fetch('https://react-hooks-project-5a6d8.firebaseio.com/ingredients.json').then(
+      response => response.json()
+    ).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    });
+    // without [] it will enter in an infinite loop decause of how useEffects work by default 
+    // [] is used so useEffects will run one time
+  }, []);
 
   function addIngredientHandler(ingredient) {
     fetch('https://react-hooks-project-5a6d8.firebaseio.com/ingredients.json', {
